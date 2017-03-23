262deaf1818c0c60b46f144a7a8725330a899f51
Przemyslaw Cholewa <przemek.cholewa.web@gmail.com>
2017-03-23 12:30:21 +0100

    express i testy

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+var app = express()
 
-var http = require('http');
-var server = http.createServer(function (req, res) {
-  res.end('hello world');
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/stock', function (req, res, next) {
+  res.json({
+    isbn: req.body.isbn,
+    count: req.body.count
+  })
+})
+
+app.get('/error', function (req, res) {
+  // res.send('Hello World!')
+  throw new Error('forced error');
+})
+
+// app.get('**', function (req, res, next) {
+//   console.error("asdfasdf");
+//   res.status(404).send("Somefing 404 ");
+// }, function (req, res) {
+//   res.send('Hello World!')
+// })
+app.use(function (req, res, next) {
+  // console.error("asdfasdf");
+  // res.status(404).send("Somefing 404 ");
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
-server.listen(3000, function () {
-  console.log("When this callback is invoked our server is listening on port: " + 3000);
+
+
+app.use(function (err, req, res, next) {
+  var status = err.status || 500;
+  res.status(status);
+  console.error(err.stack);
+  res.send('Oh no: ' + status);
 });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+// function logRequest(req, res, next) {
+//   console.log('incoming request', new Date());
+//   next();
+// }
+// function auth(req, res, next) {
+//   console.log('please your pass');
+//   next();
+// }
+// app.use(logRequest);
+// app.use(auth);
+
+
+// app.use(function (err, req, res, next) {
+//   console.error(err.stack);
+//   res.status(500).send("Somefing broke!");
+// });
