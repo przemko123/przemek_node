5486b03bc6d25b97e79b7a324f9e576cd2091c3a
Przemyslaw Cholewa <przemek.cholewa.web@gmail.com>
2017-03-23 14:49:41 +0100

    baza baza

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
 var express = require('express')
 var bodyParser = require('body-parser');
 var app = express()
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/myproject';
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+
+  // MongoClient.connect(url, function (err, db) {
+  //   console.log("Connected successfully to server");
+  //   var book = db.collection('book');
+  //   var bookArray = book.find({}).toArray(
+  //     function(err, bookArray) {
+  //       res.send(bookArray);
+  //     }
+  //   );
+  //   // console.log(bookArray);
+
+  //   db.close()
+  // });
+})
+
+app.get('/stock', function (req, res) {
+  // res.send('Hello World!')
+
+  MongoClient.connect(url, function (err, db) {
+    console.log("Connected successfully to server");
+    var book = db.collection('book');
+    var bookArray = book.find({}).toArray(
+      function (err, bookArray) {
+        res.send(bookArray);
+      }
+    );
+    // console.log(bookArray);
+
+    db.close()
+  });
 })
 
 app.post('/stock', function (req, res, next) {
+
+  MongoClient.connect(url, function (err, db) {
+    console.log("Connected stock");
+    db.collection('book')
+      .insertOne(req.body)
+
+    db.close()
+  });
+  res.json({
+    isbn: req.body.isbn,
+    count: req.body.count
+  })
+})
+app.put('/stock', function (req, res, next) {
+
+  MongoClient.connect(url, function (err, db) {
+    console.log("Connected put stock");
+    db.collection('book')
+      .updateOne({ isbn: req.body.isbn }, req.body)
+
+    db.close()
+  });
   res.json({
     isbn: req.body.isbn,
     count: req.body.count
